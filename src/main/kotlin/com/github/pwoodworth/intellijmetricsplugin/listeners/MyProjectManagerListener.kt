5d5406283c0d88aa7e2bd661f6d9bcce284896f8
GitHub Action <action@github.com>
2021-09-08 20:18:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/pwoodworth/intellijmetricsplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/pwoodworth/intellijmetricsplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.pwoodworth.intellijmetricsplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.pwoodworth.intellijmetricsplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/pwoodworth/intellijmetricsplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pwoodworth.intellijmetricsplugin.services
+
+import com.github.pwoodworth.intellijmetricsplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
